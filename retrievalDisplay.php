d9e263c2d357141d1dc80d89edc0ec5aacfc930f
anaahmedgohary <gogoahmed13@gmail.com>
2022-09-28 00:24:44 +0200

    responsive01

--- retrievalDisplay.php
+++ retrievalDisplay.php
@@ -3,10 +3,14 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>New Reviews of local bussiness add review online website</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./retrival.css" />
+    
+    
     <link rel="stylesheet" href="./style/addreview.css" />
+    <link rel="stylesheet" href="./newreviewstyle/retrival.css" />
 </head>
 
 <body>
@@ -25,12 +29,12 @@
 
         <div>
             <nav class="main-nav">
-                <p><a href="./addreview.html">home</a></p>
-                <p>latest reviews</p>
-                <a href="./retrievalDisplay.php">
-                    <p>new reviews</p>
-                </a>
-                <p>contact</p>
+                <p><a href="./addreview.html">Home</a></p>
+                <p><a href="./retrievalDisplay.php">
+                    New Reviews
+                </a></p>
+                
+                <p>Contact</p>
             </nav>
         </div>
     </header>
@@ -47,31 +51,46 @@
                     <h2 style="text-align:center;">New Reviews</h2>
                 </div>
 
-                <table class="table">
-                    <thead>
-                        <tr class="tbl-main-headrs">
+                <table class="table mtable">
+                    <!-- <thead>
+                        <tr class="tbl-main-headrs mtable-head">
 
                             <th class="col-2 mTHeadr" scope="col">review date</th>
                             <th class="col-2 mTHeadr" scope="col">Business Name</th>
                             <th class="col-1 mTHeadr" scope="col">Rating</th>
                             <th class="col-5 mTHeadr" scope="col">Review</th>
                             <th class="col-2 mTHeadr" scope="col">The Reviewer</th>
-                            <!-- <th scope="col">Time of Order</th> -->
+                             <th scope="col">Time of Order</th> 
                         </tr>
-                    </thead>
+                    </thead> -->
                     <tbody>
                         <?php include 'retrieveDb.php'; ?>
 
                         <?php if ($result->num_rows > 0) : ?>
 
                         <?php while ($array = mysqli_fetch_row($result)) : ?>
-
-                        <tr class="new-review">
-                            <td class="review-col date-col-cel"><?php echo $array[1]; ?></td>
-                            <td class="review-col rate-col-cel"><?php echo $array[2]; ?></td>
-                            <td class="review-col rev-col-cel"><?php echo $array[3]; ?></td>
-                            <td class="review-col date-col-cel"><?php echo $array[4]; ?></td>
-                            <th class="review-col name-col-cel"><?php echo $array[5]; ?></th>
+                                
+                        <tr class="new-review responsive280">
+                            <tr class="rev-div">
+                                <th class="rev-div">review date</th>
+                                <td class="rev-div"><?php echo $array[1]; ?></td>
+                            </tr>
+                            <tr class="rev-div">
+                                <th class="col-2 mTHeadr" scope="col">Business Name</th>
+                                <td class="review-col rate-col-cel"><?php echo $array[2]; ?></td>
+                            </tr>
+                            <tr class="rev-div">
+                                <th class="col-1 mTHeadr" scope="col">Rating</th>
+                                <td class="review-col rev-col-cel"><?php echo $array[3]; ?></td>
+                            </tr>
+                            <tr class="rev-div">
+                                <th class="col-5 mTHeadr" scope="col">Review</th>
+                                <td class="review-col date-col-cel"><?php echo $array[4]; ?></td>
+                            </tr>
+                            <tr class="rev-div">
+                                <th class="col-2 mTHeadr" scope="col">The Reviewer</th>
+                                <th class="review-col name-col-cel"><?php echo $array[5]; ?></th>
+                            </tr>
 
                         </tr>
 
